7ca8cb47146fcff5eb9961ff6054d558807b8774
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-18 03:43:34 +0330

    Add roboto font

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import store from "./store";
 import { Quasar } from 'quasar'
 
 // Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
 
 // Import Quasar css
