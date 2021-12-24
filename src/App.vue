bb1d15f63e37d22f38e87108d2a340cf78bd4580
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-24 05:08:01 +0330

    Setup app template, create issue template in home page and some polishing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,83 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <q-layout>
+
+    <q-toolbar>
+
+      <div>DevDIds</div>
+
+      <q-toolbar-title>
+        <!--        <q-tabs-->
+        <!--            stretch-->
+        <!--            v-model="tab"-->
+        <!--            inline-label-->
+        <!--            class="bg-purple text-white shadow-2"-->
+        <!--        >-->
+        <!--          <q-tab name="mails" icon="mail" label="Mails" />-->
+        <!--          <q-tab name="alarms" icon="alarm" label="Alarms" />-->
+        <!--          <q-tab name="movies" icon="movie" label="Movies" />-->
+        <!--        </q-tabs>-->
+      </q-toolbar-title>
+
+      <q-btn
+          class="q-mr-md"
+          rounded
+          color="pink"
+          outline
+      >
+        Connect Wallet
+      </q-btn>
+
+
+      <q-btn
+          style="background-color: white"
+          flat
+          rounded
+          icon="more_horiz"
+          aria-label="Menu"
+      >
+        <q-menu auto-close>
+          <q-list style="min-width: 100px">
+            <q-item clickable>
+              <q-item-section>New tab</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>New incognito tab</q-item-section>
+            </q-item>
+            <q-separator/>
+            <q-item clickable>
+              <q-item-section>Recent tabs</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>History</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section>Downloads</q-item-section>
+            </q-item>
+            <q-separator/>
+            <q-item clickable>
+              <q-item-section>Settings</q-item-section>
+            </q-item>
+            <q-separator/>
+            <q-item clickable>
+              <q-item-section>Help &amp; Feedback</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+
+    </q-toolbar>
+
+
+    <q-page-container class="q-layout-container overflow-hidden">
+      <router-view/>
+    </q-page-container>
+
+  </q-layout>
 </template>
 
 <style>
@@ -12,19 +86,6 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background-image: linear-gradient(#f7ebf3, #f7f8fa);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,85 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from '../components/HelloWorld.vue'
+import { ref, onMounted, reactive } from 'vue'
+
+const issue = reactive({
+  to: '',
+  subject: '',
+  data: '',
+  validFrom: '',
+  validTo: ''
+})
 </script>
 
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <q-page
+      class="row items-center justify-evenly"
+      style="min-height: 700px"
+  >
+
+    <q-card
+        class="q-px-lg q-pb-lg"
+        style="min-width: 50em; border-radius: 30px"
+        flat
+        bordered
+    >
+
+      <q-card-section>
+        Issue
+      </q-card-section>
+
+      <q-form
+          class="q-gutter-md"
+      >
+        <q-input
+            filled
+            v-model="issue.to"
+            label="To"
+            hint="Name and surname"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+
+        <q-input
+            filled
+            v-model="issue.subject"
+            label="Subject"
+            lazy-rules
+            :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+        />
+
+        <q-input
+            filled
+            v-model="issue.validFrom"
+            label="Valid From"
+            mask="date"
+            :rules="['date']"
+        >
+
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" cover transition-show="scale" transition-hide="scale">
+                <q-date v-model="issue.validFrom">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat/>
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+
+        </q-input>
+
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"/>
+        </div>
+      </q-form>
+
+    </q-card>
+  </q-page>
+</template>
