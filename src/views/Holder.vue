d583e8994d058797d27a495a915a3cc1d651db09
shima78 <shi1999mamp@gmail.com>
2021-12-25 17:25:08 +0330

    Generate Vp button Fake Data Alignment Fix

--- src/views/Holder.vue
+++ src/views/Holder.vue
@@ -1,133 +1,84 @@
 <script>
-import { ref } from 'vue'
+import { ref, onBeforeUnmount } from 'vue'
 
 
 const columns = [
   {
     name: 'name',
     required: true,
-    label: 'Dessert (100g serving)',
+    label: 'VC',
     align: 'left',
     field: row => row.name,
     format: val => `${val}`,
     sortable: true
   },
-  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
-  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-  { name: 'protein', label: 'Protein (g)', field: 'protein' },
-  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-]
+  { name: 'Issuer', align: 'center', label: 'Issuer', field: 'Issuer', sortable: true },
+  { name: 'Data', label: 'Data' , field:  'data', sortable: true },
+  { name: 'validFrom', label:'Issuance Date', field: 'validFrom' },
+  { name: 'validTo', label: 'Expiration Date', field: 'validTo' }
+ ]
 
 const rows = [
   {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%'
+    name: 'Railway Project',
+    Issuer: "0x00Cd65E9664D3cdd0855f80911a7A299cAEaC083",
+    subject: 6.0,
+    data: "Shima has Worked 3 month",
+    validFrom: 4.0,
+    validTo: 87,
   },
   {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%'
+    name: 'Google',
+    Issuer: "0x00Cd65E9664D3cdd0855f80911a7A299cAEaC083",
+    subject: 6.0,
+    data: 24,
+    validFrom: 4.0,
+    validTo: 87,
   },
   {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%'
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%'
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%'
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%'
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%'
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%'
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%'
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%'
+    name: 'Microsoft',
+    Issuer: "0x00Cd65E9664D3cdd0855f80911a7A299cAEaC083",
+    subject: 6.0,
+    data: 24,
+    validFrom: 4.0,
+    validTo: 87,
   }
+
 ]
 
 export default {
   setup () {
     const selected = ref([])
+
+    const progress = ref([
+      { loading: false, percentage: 0 },
+      { loading: false, percentage: 0 },
+      { loading: false, percentage: 0 }
+    ])
+
+    const intervals = [ null, null, null ]
+
+    function startComputing (id) {
+      progress.value[ id ].loading = true
+      progress.value[ id ].percentage = 0
+
+      intervals[ id ] = setInterval(() => {
+        progress.value[ id ].percentage += Math.floor(Math.random() * 8 + 10)
+        if (progress.value[ id ].percentage >= 100) {
+          clearInterval(intervals[ id ])
+          progress.value[ id ].loading = false
+        }
+      }, 700)
+    }
+
+    onBeforeUnmount(() => {
+      intervals.forEach(val => {
+        clearInterval(val)
+      })
+    })
     return {
+      progress,
+      startComputing,
       filter: ref(''),
       selected,
       columns,
@@ -145,17 +96,17 @@ export default {
 <template>
   <q-page
       class="row items-center justify-evenly"
-      style="min-height: 700px"
+
   >
     <q-card
         class="q-px-lg q-pb-lg"
-        style="min-width: 50em; border-radius: 30px; max-width: 70%; margin-top: 100px"
+        style="min-width: 50em; border-radius: 30px; max-width: 70%; margin-top: 70px"
         flat
         bordered
     >
   <div class="q-pa-md">
     <q-table
-        title="Treats"
+        title="Your Verifiable Credentials"
         :rows="rows"
         :columns="columns"
         row-key="name"
@@ -175,7 +126,7 @@ export default {
 
       <template v-slot:item="props">
         <div
-            class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+            class="q-pa-xs grid-style-transition"
             :style="props.selected ? 'transform: scale(0.95);' : ''"
         >
           <q-card :class="props.selected ? 'bg-grey-2' : ''">
@@ -202,9 +153,24 @@ export default {
     </q-table>
 
   </div>
-  <div class="q-mt-md">
-    Selected: {{ JSON.stringify(selected) }}
-  </div>
+
+      <div style="text-align: center">
+        <q-btn
+            :loading="progress[0].loading"
+            :percentage="progress[0].percentage"
+            color="primary"
+            @click="startComputing(0)"
+            style="width: 150px;"
+        >
+          Generate VP
+          <template v-slot:loading>
+            <q-spinner-gears class="on-left" />
+            Generating...
+          </template>
+        </q-btn>
+
+        <q-btn label="Delete" type="Delete" color="red"  style="margin-left: 5px"/>
+      </div>
     </q-card>
   </q-page>
 </template>
