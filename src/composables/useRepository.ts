7ef97fa155abe509cfea00750325f948a4f97cb4
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-27 21:58:07 +0330

    Add generateVp and verify functionalities to repository

--- src/composables/useRepository.ts
+++ src/composables/useRepository.ts
@@ -1,5 +1,6 @@
 import { useStore } from 'vuex'
 import { DevDIDs } from "../contractData/types";
+import { BigNumberish } from "ethers";
 
 export function useRepository() {
 
@@ -13,8 +14,35 @@ export function useRepository() {
         return store.getters.account
     }
 
+    async function generateVp(userVcs: BigNumberish[], validFrom: number, validTo: number) {
+        const devDIDs = store.getters.devDIDs as DevDIDs
+
+        const vp = await devDIDs.generateVp(userVcs, validFrom, validTo)
+
+        const stringVp = JSON.stringify(vp)
+        const base64Vp = btoa(stringVp)
+
+        return [base64Vp, vp]
+    }
+
+    async function verify(base64Vp: string, holderAddress: string, currentDate: number) {
+
+        const stringVp = atob(base64Vp)
+        const vp = JSON.parse(stringVp)
+
+        const devDIDs = store.getters.devDIDs as DevDIDs
+
+        return await devDIDs.verify(
+            vp,
+            holderAddress,
+            currentDate
+        )
+    }
+
     return {
         devDIDs,
-        account
+        account,
+        generateVp,
+        verify
     }
 }
