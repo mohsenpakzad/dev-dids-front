d785843dd23d729774ec440bac86aa0e3cc93ff7
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-29 02:47:43 +0330

    Add time formatting functionalities to formatting composable

--- src/composables/useFormatting.ts
+++ src/composables/useFormatting.ts
@@ -1,10 +1,25 @@
+import { date, DateLocale } from 'quasar'
+
 export function useFormatting() {
+
+    const dateFormat = 'YYYY/MM/DD'
 
     function simplifyAddress(address: string) {
         return address.substring(0, 6) + '...' + address.substring(38, 42)
     }
 
+    function timestampToStringDate(timeStamp: number): string {
+        return date.formatDate(timeStamp, dateFormat)
+    }
+
+    function stringDateToTimestamp(dataString: string): number {
+        return date.extractDate(dataString, dateFormat).valueOf()
+    }
+
     return {
-        simplifyAddress
+        dateFormat,
+        simplifyAddress,
+        timestampToStringDate,
+        stringDateToTimestamp
     }
 }
