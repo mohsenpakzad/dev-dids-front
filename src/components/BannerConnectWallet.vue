9845a9faea9ea4a7e9f3c7e46130b8aeafca3c80
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-31 00:22:42 +0330

    Add connect wallet banner component

---
+++ src/components/BannerConnectWallet.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-banner class="bg-pink text-white">
+    <template v-slot:avatar>
+      <q-icon name="cable"/>
+    </template>
+    Please connect to your wallet first!
+  </q-banner>
+</template>
