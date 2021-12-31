2e76927cd95d19db15f7ea56dbedfd22b114d7bf
Mohsen Pakzad <mohsen137853@gmail.com>
2021-12-31 17:34:09 +0330

    Switch routing to web hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 import Holder from '../views/Holder.vue'
 import Verify from '../views/Verify.vue'
@@ -36,7 +36,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
