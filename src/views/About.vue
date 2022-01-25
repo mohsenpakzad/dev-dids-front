6c2aa562f9ffefa25a86bc4a522cd47183d7eeca
Mohsen Pakzad <mohsen137853@gmail.com>
2022-01-25 20:06:21 +0330

    Fill about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <q-page>
+    <q-card
+        class="q-px-lg q-pb-lg"
+        style="width:95%;
+        max-width: 1000px;
+        border-radius: 5px;
+        margin:20px auto 20px !important;"
+        flat
+        bordered
+    >
+      <h6>Main Idea:</h6>
+      <p>
+        For today’s digital lives, digital identities are used across every app,
+        service, and device. Decentralized Identifiers (DIDs) are expected to
+        empower users to have greater control over their data.
+        For DIDs, blockchain is considered to be a platform to register identifiers
+        that users can use to identify themselves.
+        This project aims to develop a prototype of software developers’ DIDs on
+        blockchain to enable them to present verifiable claims of the past activities
+        in the belonging software development projects.
+      </p>
+      <h6>Repositories:</h6>
+      <p>
+        <q-icon name="code" size="30px"/>
+        <a
+            class="q-ml-sm"
+            href="https://github.com/mohsenpakzad/dev-dids"
+            target="_blank"
+        >
+          Backend repository
+        </a>
+      </p>
+      <p>
+        <q-icon name="smart_button" size="30px"/>
+        <a
+            class="q-ml-sm"
+            href="https://github.com/mohsenpakzad/dev-dids-front"
+            target="_blank"
+        >
+          Frontend repository
+        </a>
+      </p>
+    </q-card>
+  </q-page>
 </template>
